bf19e815de2465e0e15b192dbd4c5fab3936d356
Rahul <yourstrulyyrahul@gmail.com>
2024-03-17 12:41:09 +0530

    updated docker multiple build files script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 docker_files=("./AccountService/dockerfile" "./discovery/dockerfile" "./AuthenticationService/dockerfile" "./gateway/dockerfile" "./NotificationService/dockerfile" "./PaymentInitService/dockerfile" "./PaymentProcessingService/dockerfile")
 image_names=("account_service" "discovery_service" "authentication_service" "gateway_service" "notification_service" "payment_init_service" "payment_process_service")
 INDEX=0
-for i in $docker_files; do
-    docker build --no-cache -t "${image_names[$INDEX]}":latest . -f "${docker_files[$INDEX]}"
+for i in "${docker_files[@]}"; do
+    docker build -t --no-cache "${image_names[$INDEX]}":latest . -f "${docker_files[$INDEX]}"
     INDEX=${INDEX}+1
 done
